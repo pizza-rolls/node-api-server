0870eba9c89b43508a0b0123ee7078834da82e36
barslev <benjaminbarslev@gmail.com>
2020-03-20 16:10:01 +0100

    Updates winston to version 3.

--- test/Logger.test.js
+++ test/Logger.test.js
@@ -4,10 +4,10 @@ const winston = require('winston')
 
 describe('Logger.test.js', () => {
   it('should instantiate a winston logger', () => {
-    assert.ok(config.logger instanceof winston.Logger)
+    assert.ok(config.logger instanceof winston.createLogger().constructor.__proto__)
   })
 
   it('should use config.logger.global string to set a global var', () => {
-    assert.ok(global['myLoggerVar'] instanceof winston.Logger)
+    assert.ok(global['myLoggerVar'] instanceof winston.createLogger().constructor.__proto__)
   })
 })
